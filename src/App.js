2c1f16f24701f1e864548f78a469847cb1b1a9b0
chisommuorah <muorahmarypeace@gmail.com>
2021-08-26 18:22:52 +0100

    react setup

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div>
+        <h1> We're launching soon</h1>
+      </div>
+      <div>
+        <section>
+          <div>
+            <p></p>
+          </div>
+          <p>
+            <small>Days</small>
+          </p>
+        </section>
+        <section>
+          <div>
+            <p></p>
+          </div>
+          <p>
+            <small>Hours</small>
+          </p>
+        </section>
+        <section>
+          <div>
+            <p></p>
+          </div>
+          <p>
+            <small>Minutes</small>
+          </p>
+        </section>
+        <section>
+          <div>
+            <p></p>
+          </div>
+          <p>
+            <small>Seconds</small>
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
